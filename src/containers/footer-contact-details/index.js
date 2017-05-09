a585b203920d24199d6d8eaa88f589861b9ee84a
kateloschinina <ekaterina.loshchinina@gmail.com>
2017-05-09 11:18:45 +0100

    Clean up file.

--- src/containers/footer-contact-details/index.js
+++ src/containers/footer-contact-details/index.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-// import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class FooterContactDetails extends Component {
@@ -14,11 +13,6 @@ class FooterContactDetails extends Component {
 
   render(){
     return(
-      // <div className="o-footer__copyright kat-footer__copyright" role="contentinfo">
-      //   <small>
-      //     <abbr title={this.props.footerCopyright.abbr.title} aria-label={this.props.footerCopyright.abbr.abbr}> {this.props.footerCopyright.abbr.label} </abbr> {this.props.footerCopyright.abbr.text}. {this.props.footerCopyright.footer} <a href={this.props.footerCopyright.footerLink.link} aria-label="F T Editorial Code of Practice">{this.props.footerCopyright.footerLink.text}</a>
-      //   </small>
-      // </div>
       <div className="kat-footer__contact-details">
         <p className="kat-footer__contact-details-item">{this.props.footerContactDetails.intro}</p>
         <p className="kat-footer__contact-details-item"><strong>{this.props.footerContactDetails.ukPhoneNumber}</strong>{this.props.footerContactDetails.or}<strong>{this.props.footerContactDetails.usPhoneNumber}</strong></p>
@@ -40,4 +34,3 @@ const mapStateToProps = (store) => {
 };
 
 export default connect(mapStateToProps)(FooterContactDetails);
-// export default FooterContactDetails;
