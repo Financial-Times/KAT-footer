d2177327821e52d6b0e3453d8fb166f422c63f91
kateloschinina <ekaterina.loshchinina@gmail.com>
2017-05-10 16:59:47 +0100

    Failing test to send to origami team.

---
+++ src/containers/kat-footer/test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import KatFooterContainer from './index.js';
+
+describe('KatFooterContainer', () => {
+
+  const wrapper = mount(<KatFooterContainer />);
+});
