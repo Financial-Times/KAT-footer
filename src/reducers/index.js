0a5fe416fe9d5db1e3e84024a21235a62e725719
dapenguin <anthony.jeffery@littleblueweb.com>
2017-07-20 12:36:11 +0100

    Added tracking data to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,26 +9,32 @@ const defaultFooterState = {
 	"legalLinks": {
 		"links": [
 			{
+				"data-trackable": "terms-and-conditions",
 				"href": "http://help.ft.com/help/legal-privacy/terms-conditions/",
 				"label": "Terms & Conditions"
 			},
 			{
+				"data-trackable": "slavery-statement",
 				"href": "https://help.ft.com/help/legal/slavery-statement/",
 				"label": "Slavery Statement"
 			},
 			{
+				"data-trackable": "privacy",
 				"href": "http://help.ft.com/help/legal-privacy/privacy/",
 				"label": "Privacy"
 			},
 			{
+				"data-trackable": "cookies",
 				"href": "http://help.ft.com/help/legal-privacy/cookies/",
 				"label": "Cookies"
 			},
 			{
+				"data-trackable": "copyright",
 				"href":"http://help.ft.com/help/legal-privacy/copyright/copyright-policy/",
 				"label": "Copyright"
 			},
 			{
+				"data-trackable": "help",
 				"href": "http://help.ft.com/help/b2b-support/knowledge-administration-tool/",
 				"label": "Help"
 			}
