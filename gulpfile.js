ca2abba10c3621a2e868a5cbf2cfff9129197f20
Simon Legg <leggsimon@gmail.com>
2017-11-09 17:14:23 +0000

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-require('dotenv').config({silent: true});
 const gulp = require('gulp');
 const obt = require('origami-build-tools');
 const nodemon = require('gulp-nodemon');
@@ -7,22 +6,19 @@ const size = require('gulp-size');
 const livereload = require('gulp-livereload');
 let appServer;
 
-
-gulp.task('build', function() {
+gulp.task('build', function () {
   return obt.build(gulp, {
-    js: './src/index.js',
-    sass: './style/main.scss',
-    buildJs: 'bundle.js',
-    buildCss: 'kat-footer.css',
+    sass: './main.scss',
+    buildCss: 'main.css',
     buildFolder: 'public',
     env: process.env.NODE_ENV
   });
 });
 
-gulp.task('build-page', function() {
+gulp.task('build-page', function () {
   return obt.build(gulp, {
-    sass: './style/page.scss',
-    buildCss: 'page.css',
+    sass: './main.scss',
+    buildCss: 'main.css',
     buildFolder: 'public',
     env: process.env.NODE_ENV
   });
@@ -32,13 +28,13 @@ gulp.task('install', function () {
   return obt.install();
 });
 
-gulp.task('test', function() {
+gulp.task('test', function () {
   return obt.test.npmTest(gulp);
 });
 
-gulp.task('serve', ['dev-add-livereload', 'build', 'build-page'], function(){
+gulp.task('serve', ['dev-add-livereload', 'build', 'build-page'], function (){
   appServer = nodemon({
-    'script': 'server.js',
+    'script': 'demos/app.js',
     'verbose': true,
     'watch': false,
     'ignore': ['*.*']
@@ -52,13 +48,13 @@ gulp.task('restart-server', function () {
   appServer.restart();
 });
 
-gulp.task('refresh-page', ['build', 'build-page'], function() {
+gulp.task('refresh-page', ['build', 'build-page'], function () {
   livereload.changed('src/index.js');
 });
 
 gulp.task('watch', ['serve'], function () {
-  livereload.listen({port: process.env.LIVERELOAD_PORT});
-  gulp.watch(['./style/**/*', './src/**/*'], ['refresh-page']);
+  livereload.listen({ port: process.env.LIVERELOAD_PORT });
+  gulp.watch(['./**/*.scss', './demos/**/*'], ['refresh-page']);
   gulp.watch('./*.*', ['restart-server']);
 });
 
